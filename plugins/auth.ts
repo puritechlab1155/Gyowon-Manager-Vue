a816a86238ad3458922c0b6f34437a3f6638045f
Dongmin <emthrology@gmail.com>
2025-05-29 15:01:53 +0900

    api auth 세팅

---
+++ plugins/auth.ts
@@ -0,0 +1,11 @@
+import { useUser, fetchCurrentUser } from '@/composables/useAuth';
+
+export default defineNuxtPlugin(async (nuxtApp) => {
+  const user = useUser();
+
+  // Skip if already initialized on server
+  if (user.value !== undefined) return;
+  const userData = await fetchCurrentUser();
+  console.log(userData, 'userData in auth.ts');
+  user.value = userData;
+});
